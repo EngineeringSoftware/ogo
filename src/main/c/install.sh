bb7323e64f3993ab4e7f5a60adc56d08e9e6ef3b
Michael Zhao <michaelzhao820@gmail.com>
2026-01-27 11:11:51 -0600

    upgrade to conan 2.

--- src/main/c/install.sh
+++ src/main/c/install.sh
@@ -7,16 +7,10 @@ BUILD_TYPE=$1
 rm -rf build && mkdir build && cd build
 
 # install antlr using conan package manager
-conan install ../external/conanfile.txt -if . -of .
-
-# set environment for env variables
-source activate.sh
+conan install ../external/conanfile.py -of . --build=missing
 
 # execute cmake to generate Makefile
 cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_MODULE_PATH=$(pwd) -DCMAKE_TOOLCHAIN_FILE=$(pwd)/conan_toolchain.cmake
 
 # build native agent
 make
-
-# deactivate environment
-source deactivate.sh
