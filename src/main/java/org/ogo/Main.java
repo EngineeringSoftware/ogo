3ff92e7ba031f29d1b533d26089a35b152fb5338
Michael Zhao <michaelzhao820@gmail.com>
2026-02-07 17:41:27 -0600

    Clean main.java to use logger and remove random prints

--- src/main/java/org/ogo/Main.java
+++ src/main/java/org/ogo/Main.java
@@ -1,17 +1,22 @@
 package org.ogo;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
 import org.ogo.client.OGO;
+
 
 public class Main {
 
+  private static final Logger logger = Logger.getLogger(Main.class.getName());
+
   public static void main(String[] args) throws Exception {
     if (args.length != 1) {
-      System.out.println("No query given");
+      logger.severe("No query given");
       System.exit(1);
     }
 
     Thread.sleep(1000);
-    System.out.println("abc");
+    logger.info("Initializing OGO");
     OGO.init();
     OGO.setWhiteList("java/lang/Object", "org.ogo.Main", "org/ogo/Main", "org/ogo", "org.ogo");
     OGO.clearDatabase = false;
@@ -19,20 +24,19 @@ public class Main {
     OGO.followRoot = true;
     // OGO.whitelist = true;
     OGO.setExcludeFromBlackList("java/lang/Object");
-    System.out.println("def");
 
     // public static Object[] query(Object root, String cQuery, Object... objects)
     Object obj = new Main();
     Object[] results = OGO.query(obj, "match (n) return count (n)");
-    System.out.println(results);
+    logger.info("Results: " + Arrays.toString(results));
     if (results != null) {
-      System.out.println(results.length);
+      logger.info("Result count: " + results.length);
       for (Object o : results) {
-        System.out.println(o);
+        logger.info(String.valueOf(o));
       }
     }
 
     // String query = args[0];
-    // System.out.println(query);
+    // logger.info(query);
   }
 }
